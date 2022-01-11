70bd33088e091a5e08c435123736270a17e7221e
Babin Bohora <bohorababin1@gmail.com>
2022-01-11 21:15:29 +0900

    restaurant actions done and working with views

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,27 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurants_path
+    else
+      render :new
+    end
+  end
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).get(:name, :address, :phone_number, :category)
+  end
 end
