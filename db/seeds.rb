318cdc5dc7d706864ccfacff8fa2abc14d4da9cb
Babin Bohora <bohorababin1@gmail.com>
2022-01-11 20:21:09 +0900

    seeding done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "deleting all records"
+Restaurant.destroy_all
+Review.destroy_all
+
+restaurants = []
+puts "seeding the database with restaurant"
+10.times do
+  restaurants << Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_name,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+    phone_number: Faker::PhoneNumber.cell_phone
+  )
+end
+
+puts "seeding the database with review"
+20.times do
+  Review.create(
+    content: Faker::Restaurant.review,
+    restaurant: restaurants.sample,
+    rating: rand(0..5)
+  )
+end
+
+puts "done done"
